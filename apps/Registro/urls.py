5624dfcce72a025a7bfc94a6910cb0912c09b628
abril castro <agata__@hotmail.cl>
2020-11-08 17:13:59 -0300

    agregado crud completo para producto con template correspondientes

--- apps/Registro/urls.py
+++ apps/Registro/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     # listar los productos de la bd
     path('listarProductos', views.listar_productos, name="listar_productos"),
     path('add_producto', views.ProductoCreate.as_view(), name="add_producto"),
+    path('list_productos/', views.ProductoList.as_view(), name='list_productos'),
+    path('edit_producto/<int:pk>', views.ProductoUpdate.as_view(), name='edit_producto'),
+    path('del_producto/<int:pk>', views.ProductoDelete.as_view(), name='del_producto'),
 ]
--- apps/Registro/views.py
+++ apps/Registro/views.py
@@ -3,6 +3,7 @@ from .models import Producto, Categoria
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .forms import ProductoForm
+# from django.db.models import Q 
 
 # Create your views here.
 
@@ -19,4 +20,35 @@ class ProductoCreate(CreateView):
     template_name = 'Registro/producto_form.html'
     success_url = reverse_lazy("listar_productos")
     
-  
+class ProductoList(ListView):
+    model = Producto
+    template_name = 'Registro/list_productos.html'
+
+
+class ProductoUpdate(UpdateView):
+    model = Producto
+    form_class = ProductoForm
+    template_name = 'Registro/producto_form.html'
+    success_url = reverse_lazy('list_productos')
+
+
+class ProductoDelete(DeleteView):
+    model = Producto
+    template_name = 'Registro/producto_delete.html'
+    success_url = reverse_lazy('list_productos')
+    
+#--------------- filtros --------------------------------
+# class BuscarAlumnosView(ListView):
+#     model = Alumno
+#     template_name = 'Registro/buscar_alumnos.html'
+
+# class SearchResultsView(ListView):
+#     model = Alumno
+#     template_name = 'Registro/search_results.html'
+    
+#     def get_queryset(self): 
+#         query = self.request.GET.get('q')
+#         object_list = Alumno.objects.filter(
+#             Q(domicilio__icontains=query))
+        
+#         return object_list
